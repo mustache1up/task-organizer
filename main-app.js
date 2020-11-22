f6d17f89045b49e2461637671d8944e477ff80b4
mustache1up <daniel.sp@msn.com>
2020-11-22 19:42:39 -0300

    add task list

--- main-app.js
+++ main-app.js
@@ -1,13 +1,59 @@
 var mainApp = new Vue({
   el: '#main-app',
   data: {
+    topics: [
+      {
+        title: "Work Things",
+        missions: [
+          {
+            title: "Make a to-do list webapp",
+            tasks: [
+              {
+                title: "Create first draft",
+                status: "done"
+              },
+              {
+                title: "Initialize repository",
+                status: "freeze"
+              },
+              {
+                title: "Commit base template",
+                status: "next"
+              }
+            ]
+          },
+          {
+            title: "Choose git hosting",
+            tasks: [
+              {
+                title: "List top 5 git hosts",
+                status: "done"
+              },
+              {
+                title: "Compare features",
+                status: "freeze"
+              },
+              {
+                title: "Write down findings",
+                status: "next"
+              }
+            ]
+          }
+        ]
+      }
+    ]
 
   },
   mounted: function () {
 
   },
   methods: {
-
+    addTask: function (mission) {
+      mission.tasks.push({
+        title: "",
+        status: "todo"
+      })
+    }
   },
   filters: {
 
